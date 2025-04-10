d1655076cec28bf0028e40eb173a668a95e7a301
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2025-04-10 09:10:56 +0200

    aufgabe eins geändert

--- task-1-read-files/reads.js
+++ task-1-read-files/reads.js
@@ -1,14 +1,10 @@
-import {readFile} from "fs";
+import fs from "fs";
 
 function printFile(filename) {
-    readFile(filename, "utf-8", function (err, data) {
-        if(err) {
-            console.error(`Fehler beim Lesen von ${filename}:`, err.message);
-        } else {
-            console.log(data);
-        }
-    });
+    const readMe = fs.readFileSync(filename, "utf-8");
+    console.log(readMe);
 }
+
 
 printFile("data1.txt");
 printFile("data3.txt");
